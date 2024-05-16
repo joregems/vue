7c0a28e53a9698b2892ebd04dcfe5fd5161c0846
joregems <joregems@gmail.com>
2024-05-16 05:32:57 +0000

    the sidebar menu is implemented using emit and using events, seems better using an store

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,68 @@
 <template>
+  {{ active_sidebar }}
+  <v-card>
+    <v-layout>
 
-  <v-app class="">
-    <TopNavBar class="" width="80" :active_sidebar="active_sidebar" @toggle="() => { active_sidebar = !active_sidebar }">
-    </TopNavBar>
-    <!-- <div class="caja1"></div>
-    <div class="caja2"></div> -->
-    <v-dialog v-model="conection_down" fullscreen full-width>
-      waiting the server
-      <v-container fluid fill-height style="background-color: rgba(255, 255, 255, 0.5);">
-        <v-layout justify-center align-center>
-          <v-progress-circular indeterminate color="primary">
-          </v-progress-circular>
-        </v-layout>
-      </v-container>
-    </v-dialog>
-    <v-main class="" style="min-height: 300px;">
-      <router-view />
-    </v-main>
-  </v-app>
-  <SideNavBar class="" :active_sidebar="active_sidebar" @toggle="()=>{active_sidebar=!active_sidebar}"></SideNavBar>
+      <v-app class="">
+        <TopNavBar class="" width="80" :active_sidebar="active_sidebar"
+          @toggle="() => {active_sidebar=false;active_sidebar=true}">
+        </TopNavBar>
+
+        <v-dialog v-model="conection_down" fullscreen full-width>
+          waiting the server
+          <v-container fluid fill-height style="background-color: rgba(255, 255, 255, 0.5);">
+            <v-layout justify-center align-center>
+              <v-progress-circular indeterminate color="primary">
+              </v-progress-circular>
+            </v-layout>
+          </v-container>
+        </v-dialog>
+        <v-main class="">
+          <router-view />
+        </v-main>
+      </v-app>
+      <SideNavBar :active_sidebar="active_sidebar" @toggle=" active_sidebar = $event">
+      </SideNavBar>
+    </v-layout>
+
+  </v-card>
 
 </template>
+
+<!-- <template>
+  <v-app class="">
+    <v-card>
+      <v-layout>
+        <v-app-bar color="primary" prominent>
+          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>My files</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <template v-if="$vuetify.display.mdAndUp">
+            <v-btn icon="mdi-magnify" variant="text"></v-btn>
+
+            <v-btn icon="mdi-filter" variant="text"></v-btn>
+          </template>
+
+          <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" :location="undefined" temporary>
+          <v-list :items="items"></v-list>
+        </v-navigation-drawer>
+
+        <v-main style="height: 500px;">
+          <router-view />
+        </v-main>
+      </v-layout>
+    </v-card>
+
+
+  </v-app> 
+</template>-->
+
 
 <script setup>
 import '@/normalize.css'
@@ -33,9 +75,15 @@ const router = useRouter();
 // router.addRoute({ path: '/about2', component: HomeView })
 const logo = "@/assets/logo.svg";
 import { useAuthStore } from '@/stores/AuthStore';
+
+
 const authStore = useAuthStore();
 const active_sidebar = ref(false)
+const reasignate =()=>{
+  active_sidebar.value=true;
+  
 
+}
 // router.addRoute({ path: '/about2', component: HomeView });
 const conection_down = ref(true);
 
@@ -47,20 +95,27 @@ const conection_down = ref(true);
 })();
 </script>
 <style>
-/* body {
-  overflow-y: hidden;
-  border: 4px solid blueviolet;
-  background-color: blueviolet;
-  width: 100%;
-  margin: 0;
-  height: 100%;
-  overflow: hidden
+/* .v-application__wrap {
+  min-height: 0vh !important;
 } */
 
-/* #app {
+/* body {
+  overflow-x: hidden;
+  border: 4px solid blueviolet;
+  background-color: blueviolet;
+  width: 100vw;
+  margin: 0;
+  height: 100vh;
+  overflow: hidden;
+  scroll-behavior: smooth;
+} */
+
+#app {
   border: 4px solid rgb(90, 212, 9);
   background-color: rgb(17, 230, 163);
-} */
+  width: 100%;
+  height: 100%;
+}
 </style>
 <style scoped>
 /* .full {
--- front/src/components/SideNavBar.vue
+++ front/src/components/SideNavBar.vue
@@ -1,48 +1,52 @@
 <template>
-  <div class="caja1"></div>
-  <v-card class="custom-side-bar">
-    <v-layout>
-      <v-navigation-drawer
-      v-model="props.active_sidebar"
-      image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-      theme="dark"
-      temporary
-      >
-        <v-list nav>
-          <div>
-            <v-list-item>
-            <v-btn icon class="custom-side-bar__icon" @click.stop="$emit('toggle')">
+  <v-navigation-drawer v-model="child_active_sidebar" image="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+    theme="dark" temporary>
+    <v-list nav>
+      <div>
+        <v-list-item>
+          <v-btn icon class="custom-side-bar__icon" @click.stop="$emit('toggle', !child_active_sidebar)">
 
-              <v-icon>mdi-menu-close</v-icon>
-            </v-btn>
-          </v-list-item>
-            <v-spacer></v-spacer>
+            <v-icon>mdi-menu-close</v-icon>
+          </v-btn>
+        </v-list-item>
+        <v-spacer></v-spacer>
 
-          </div>
-          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors"
-            value="supervisors"></v-list-item>
-          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main style="height: 250px"></v-main>
-    </v-layout>
-  </v-card>
+      </div>
+      <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+      <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+      <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+  <!-- <v-main style="height: 250px"></v-main> -->
 </template>
 <script setup>
-const props = defineProps({active_sidebar:Boolean});
+import { ref, watch } from 'vue';
+import { defineEmits } from 'vue';
+const props = defineProps({ active_sidebar: Boolean });
+const child_active_sidebar = ref(props.active_sidebar);
+const emit = defineEmits(['toggle'])
+
+watch(() => props.active_sidebar, (new_active_sidebar, old_active_sidebar) => {
+  // alert("props.active_sidebar");
+  child_active_sidebar.value = new_active_sidebar;
+})
+
+watch(child_active_sidebar, (new_child_active_sidebar, old_child_active_sidebar) => {
+  if (!new_child_active_sidebar&&old_child_active_sidebar){
+    emit('toggle', new_child_active_sidebar)
+
+  }
+  // alert("child_active_sidebar"+new_child_active_sidebar+ old_child_active_sidebar)
+ })
 </script>
 <style scoped>
 .custom-side-bar {
+  border: 2px solid red;
 
-  background-color: blue;
-  /* border: 2px solid cyan; */
+  /* background-color: red;
   z-index: 2000;
   transition: all 10;
-  /* transition: 200ms linear 50ms; */
-
-/* Global values */
-/* transition: inherit; */
+  transition: 200ms linear 50ms; */
 }
 
 .custom-side-bar__icon {
@@ -50,7 +54,7 @@ const props = defineProps({active_sidebar:Boolean});
   transform: rotate(180deg);
   position: absolute;
   right: 0;
-  align-items:'end';
+  align-items: 'end';
   z-index: 100;
 }
 
